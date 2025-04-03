0f28ea63b952b02bfbb057bce4cf63f42ed10496
mitchell-mccracken <mpm5069@gmail.com>
2025-04-03 15:24:12 -0400

    Updated some logic in main function

--- createValidation.py
+++ createValidation.py
@@ -11,25 +11,57 @@ def create_validation(input_text, dto_name):
     line = line.replace('val ', '')
     field = line.split("=")[0].strip()
 
+    length = None
     if 'varchar' in line:
       length = line.split("varchar(")[1].split(')')[0].split(',')[1].strip()
     elif 'char' in line:
       length = 1
+    else:
+      continue
       
-    output_line = f'{dto_name}::{field} ifPresent {{ maxLength({length})}}'
+    output_line = f'{dto_name}::{field} ifPresent {{ maxLength({length}) }}'
     print(output_line)
     
     # print(f'field: {field} | length {length}')
 
 
 input_text = """
-val ethnicity = varchar("ETHNICITY", 100).nullable()
-    val language = varchar("LANGUAGE", 100).nullable()
-    val referralSource = varchar("REFERRAL_SOURCE", 200).nullable()
-    val sailingExp = varchar("SAILING_EXP", 100).nullable()
-    val organization = varchar("ORGANIZATION", 100).nullable()
-    val orgContact = varchar("ORG_CONTACT", 200).nullable()
-    val student = char("STUDENT").nullable()
+    val assignId = integer("ASSIGN_ID").autoIncrement("PERSONS_MEMBERSHIPS_SEQ")
+    override val id = assignId.entityId()
+    override val primaryKey = PrimaryKey(id, name = "PERSONS_MEMBERSHIPS_PK")
+    val personId = reference("PERSON_ID", Persons)
+    val membershipTypeId = reference("MEMBERSHIP_TYPE_ID", MembershipTypes)
+    val purchaseDate = datetime("PURCHASE_DATE").nullable()
+    val startDate = datetime("START_DATE").nullable()
+    val expirationDate = datetime("EXPIRATION_DATE").nullable()
+    val schoolId = reference("SCHOOL_ID", HighSchools).nullable()
+    val orderId = reference("ORDER_ID", OrderNumbers).nullable()
+    val price = integer("PRICE")
+    val createdOn = datetime("CREATED_ON")
+    val createdBy = varchar("CREATED_BY", 500).nullable()
+    val updatedOn = datetime("UPDATED_ON")
+    val updatedBy = varchar("UPDATED_BY", 500).nullable()
+    val temp = char("TEMP", 1)
+    val paymentLocation = varchar("PAYMENT_LOCATION", 50).nullable()
+    val paymentMedium = varchar("PAYMENT_MEDIUM", 50).nullable()
+    val discountId = integer("DISCOUNT_ID").nullable()
+    val oldCardNum = varchar("OLD_CARD_NUM", 100).nullable()
+    val oldCompReason = varchar("OLD_COMP_REASON", 500).nullable()
+    val oldDmgWaiver = char("OLD_DMG_WAIVER", 1).nullable()
+    val closeId = reference("CLOSE_ID", FoCloses).nullable()
+    val ccTransNum = integer("CC_TRANS_NUM").nullable()
+    val groupId = reference("GROUP_ID", JpGroups).nullable()
+    val discountInstanceId = reference("DISCOUNT_INSTANCE_ID", DiscountInstances).nullable()
+    val notes = varchar("NOTES", 4000).nullable()
+    val voidCloseId = reference("VOID_CLOSE_ID", FoCloses).nullable()
+    val discountAmt = integer("DISCOUNT_AMT").nullable()
+    val pretaxPrice = integer("PRETAX_PRICE").nullable()
+    val taxRate = integer("TAX_RATE").nullable()
+    val ignoreDiscountFrozen = char("IGNORE_DISCOUNT_FROZEN", 1).nullable()
+    val originalExpirationDate = datetime("ORIGINAL_EXPIRATION_DATE").nullable()
+    val covidEmailSent = char("COVID_EMAIL_SENT", 1).nullable()
+    val covidDays = integer("COVID_DAYS").nullable()
+    val compassOrderID = reference("COMPASS_ORDER_ID", CompassOrders).nullable()
 """
 
-create_validation(input_text, "PersonUpdateDTO")
+create_validation(input_text, "MembershipCreateDTO")
